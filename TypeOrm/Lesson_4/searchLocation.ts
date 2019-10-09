466798b9eabd01c49732a1827710c92ccd099017
Stanislav Volnjanskij <stas@kultprosvet.net>
2019-10-09 08:42:46 +0300

    TypeOrm / lesson 4 /location search fixes

--- TypeOrm/Lesson_4/searchLocation.ts
+++ TypeOrm/Lesson_4/searchLocation.ts
@@ -7,7 +7,7 @@ import {
 import * as faker from 'faker'
 import { PointEntity } from './PointEntity'
 function toRad(grad: number): number {
-    return (grad / 360) * 2 * Math.PI
+    return (grad / 180) * Math.PI
 }
 function toGrad(rad: number): number {
     return (rad / (2 * Math.PI)) * 360
@@ -17,31 +17,33 @@ async function runQuery() {
     const R = 6371
     const dist = 400
 
-    const latitude = 48.4622135
-    const longitude = 34.8602749
+    const longitude = 35
+    const latitude = 48
 
     const deltaLat = toGrad(dist / R)
 
     const deltaLon = toGrad(
         dist / (R * Math.cos(toRad(latitude))),
     )
+
     //prettier-ignore
     const polygon = `POLYGON((
-     ${latitude - deltaLat} ${longitude - deltaLon},
-     ${latitude - deltaLat} ${longitude + deltaLon},
-     ${latitude + deltaLat} ${longitude + deltaLon},
-     ${latitude + deltaLat} ${longitude - deltaLon},
-     ${latitude - deltaLat} ${longitude - deltaLon}
+      ${longitude - deltaLon} ${latitude - deltaLat},
+      ${longitude - deltaLon} ${latitude + deltaLat},
+      ${longitude + deltaLon} ${latitude + deltaLat},
+      ${longitude + deltaLon} ${latitude - deltaLat},
+      ${longitude - deltaLon} ${latitude - deltaLat} 
     ))`
 
     let res = await createQueryBuilder(PointEntity)
         .select(
-            `ST_Distance(location, ST_SRID(Point(${latitude},${longitude}),4326))/1000 as distance`,
+            `ST_Distance(location, ST_SRID(Point(${longitude},${latitude}),4326),"kilometre") as distance`,
         )
+        .addSelect('ST_ASTEXT(location) as location')
         .where(
             `ST_CONTAINS(ST_GEOMFROMTEXT("${polygon}",4326),location)`,
         )
-        .having(`distance < ${dist * 1000}`)
+        .having(`distance < ${dist}`)
         .orderBy('distance', 'ASC')
         .getRawMany()
 
